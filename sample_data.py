1b260cfd7eff4a77aaf53f98b6eea9bdb486b1ac
dragonbotdevofficials <officials998877@gmail.com>
2026-06-15 17:58:05 +0530

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,10 @@
+ORDERS = {
+    "ORDER123": {
+        "status": "In Transit",
+        "estimated_delivery": "2 days"
+    },
+    "ORDER456": {
+        "status": "Delivered",
+        "estimated_delivery": "Completed"
+    }
+}
